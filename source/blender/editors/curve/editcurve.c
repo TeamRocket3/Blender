39110b51a692dbd5fdca730446dc0cc1a0aebad9
roni2006 <38332662+roni2006@users.noreply.github.com>
2018-12-12 22:12:10 -0500

    Update editcurve.c

--- source/blender/editors/curve/editcurve.c
+++ source/blender/editors/curve/editcurve.c
@@ -6063,7 +6063,7 @@ int join_curve_exec(bContext *C, wmOperator *op)
 	CTX_DATA_END;
 
 	/* that way the active object is always selected */
-	if (ok == false) {
+	if (!ok) {
 		BKE_report(op->reports, RPT_WARNING, "Active object is not a selected curve");
 		return OPERATOR_CANCELLED;
 	}
